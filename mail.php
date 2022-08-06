ee0a093b9f62d8716e4c71dd2995e1128b5e76c1
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2022-08-06 14:21:17 +0530

    Navbar Fixed

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $name = $_POST['fName'];
 $email= $_POST['email'];
 $contact= $_POST['phone'];
 
-$to = "shantanugupta135@gmail.com";
+$to = "realtyavenuesales@gmail.com";
 $subject = "DLF MAILER";
 
 // $txt ="Please find the mentioned details of a intersted user in DLF project  \r\n  Name = ". $name . "\r\n  Email = " . $email . "\r\n contact =" . $contact . "\r\n Typology =" . $typology . "\r\n Location =" . $location . "\r\n Budget =" . $budget;
